a5232afd0d32edb5952d0277f58e2c6e004da7ee
nasirhm <nasirhussainm14@gmail.com>
2018-10-09 17:45:38 -0500

    JSX first Lesson

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -4,23 +4,33 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var a = "Hello";
+    var style = {fontSize:"20px"};
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+     // <div>
+     // </div>
+     <div>
+        <p style={style}>Hey World</p>
+        <div>
+          <p>{a}</p>
+        </div>
+     </div>
     );
   }
 }
